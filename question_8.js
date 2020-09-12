687d4a7087b93a0916145c4a2467c708cc1209c8
akshay rajale <akshay.rajale93@gmail.com>
2020-09-11 19:50:29 -1200

    Answer

--- question_8.js
+++ question_8.js
@@ -1,8 +1,7 @@
 // Assume there is a json object of this format
 
-var obj = {
-“id” : 4, “name” : “abc”,
-“id” : 10, “name” : “ab2”,
-“id” : 5, “name” : “abc3”,
-“id” : 6, “name” : “abc5”
-}
+items.sort(function(a, b) {
+
+    return a.id - b.id
+    
+    });
